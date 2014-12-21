16cc8caa079a1dbf0629d855d840621ccbb6e23d
Grégoire <gregoire.mesnil@laposte.net>
2014-12-20 20:19:16 -0500

    data.sh finished

--- scripts/data.sh
+++ scripts/data.sh
@@ -9,14 +9,25 @@ wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 tar -xvf aclImdb_v1.tar.gz
 
 for j in train/pos train/neg test/pos test/neg train/unsup; do
-  rm temp
   for i in `ls aclImdb/$j`; do cat aclImdb/$j/$i >> temp; awk 'BEGIN{print;}' >> temp; done
   normalize_text temp
   mv temp-norm aclImdb/$j/norm.txt
+  rm temp
 done
 
-mv aclImdb/train/pos/norm.txt train-pos.txt
-mv aclImdb/train/neg/norm.txt train-neg.txt
-mv aclImdb/test/pos/norm.txt test-pos.txt
-mv aclImdb/test/neg/norm.txt test-neg.txt
-mv aclImdb/train/unsup/norm.txt train-unsup.txt
+mkdir data
+mv aclImdb/train/pos/norm.txt data/full-train-pos.txt
+mv aclImdb/train/neg/norm.txt data/full-train-neg.txt
+mv aclImdb/test/pos/norm.txt data/test-pos.txt
+mv aclImdb/test/neg/norm.txt data/test-neg.txt
+mv aclImdb/train/unsup/norm.txt data/train-unsup.txt
+
+cd data
+head -n 10000 full-train-pos.txt > small-train-pos.txt
+head -n 10000 full-train-neg.txt > small-train-neg.txt
+tail -n 2500 full-train-pos.txt > valid-pos.txt
+tail -n 2500 full-train-neg.txt > valid-neg.txt
+cd ..
+
+rm -r aclImdb
+rm aclImdb_v1.tar.gz
