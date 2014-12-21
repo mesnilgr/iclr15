81364a1012962ab123f75b268ad0bfce6831d92c
Grégoire <gregoire.mesnil@laposte.net>
2014-12-20 20:30:23 -0500

    main script

---
+++ go.sh
@@ -0,0 +1,13 @@
+chmod +x scripts/*
+cd ..
+mkdir iclr15_run; cd iclr15_run
+mkdir scores
+
+#./../iclr15/scripts/data.sh
+#./../iclr15/scripts/install_liblinear.sh
+./../iclr15/scripts/rnnlm.sh
+#./../iclr15/scripts/paragraph.sh
+#./../iclr15/scripts/nbsvm.sh
+#./../iclr15/scripts/ensemble.sh
+
+cd ./../iclr15
