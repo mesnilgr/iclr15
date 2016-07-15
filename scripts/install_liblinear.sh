d8f487494d35a007e833713a6468eac0f39e06cb
Grégoire Mesnil <gregoire@Gregoires-Air.lan>
2016-07-15 13:57:00 +0200

    fix liblinear download

--- scripts/install_liblinear.sh
+++ scripts/install_liblinear.sh
@@ -1,4 +1,4 @@
-wget http://www.csie.ntu.edu.tw/~cjlin/liblinear/liblinear-1.96.zip
+wget https://www.csie.ntu.edu.tw/~cjlin/liblinear/oldfiles/liblinear-1.96.zip
 unzip liblinear-1.96.zip
 rm liblinear-1.96.zip
 cd liblinear-1.96
